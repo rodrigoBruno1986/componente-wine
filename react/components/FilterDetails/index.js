b84a77acdef4a98f7673cc656a8525ed6b4b5c50
rbenitez <rbenitez@glamit.com.ar>
2021-09-16 04:53:17 -0300

    cambios en el componente filterDetail

--- react/components/FilterDetails/index.js
+++ react/components/FilterDetails/index.js
@@ -1,62 +1,31 @@
 import React, {useContext} from 'react'
-import {useCssHandles} from 'vtex.css-handles'
 import {ProductContext} from 'vtex.product-context'
-import './style.css'
+import styles from './style.css'
 
-/// ACA SE ESTA CREANDO LAS CLASES DE CSS
-const CSS_HANDLES = [
-    'FilterDetails--container',
-    'FilterDetails--detailsGroupOne',
-    'FilterDetails--detailsGroupTwo',
-    'FilterDetails--detailsItem',
-    'FilterDetails--detailsItem-2',
-    'FilterDetails--text-1',
-    'FilterDetails--text-2'
-];
+
 const FilterDetails = (props) => {
-    //// ESTA CONSTANTE HANDLES NOS ESTA TRAYENDO LAS CLASES DE CSS
-    const handles = useCssHandles(CSS_HANDLES);
+    
+   
 
-    const valuesFromContext = useContext(ProductContext);
-    let product = valuesFromContext.product;
-
-    // VARIABLE QUE NOES ESTA TRAYENDO LAS ESPECIFICACIONES DEL PRODUCTO
-    let grupoName = product.specificationGroups[0]?.specifications;
-
-
-    //// SEPARAMOS POR GRUPO 1 Y GRUPO 2
-    function skfG(groupName, numeroDeGrupo) {
-        if(groupName) {
-            /// ESTE IF NOS ESTA MOSTRANDO LAS  ESPECIFICAIONES ESPECIALES QUE QUEREMOS SEPARAR DEL RESTO
-            if(numeroDeGrupo === 1) {
-                return groupName.map((valor, i) => (
-                    (valor.name === 'Motor' || valor.name === 'Modelo' || valor.name === 'Año' || valor.name === 'Marca2' || valor.name === 'Marca' || valor.name === 'Código de Aplicación') && (
-                        <div className={handles['FilterDetails--detailsItem']} key={`${valor.name}-${valor.value}-${i}`}>
-                            <p className={handles['FilterDetails--text-1']}>{valor.name}</p>
-                            <p className={handles['FilterDetails--text-2']}>{valor.values}</p>
-                        </div>  
-                    )
-                ))
-            }
-
-            /// ESTE IF NOS ESTA MOSTRANDO EL RESTO DE LAS ESPECIFICACIONES DEL PRODUCTO
-            if(numeroDeGrupo === 2) {
-                return groupName.map((valor, i) => (
-                    (valor.name !== 'Motor' && valor.name !== 'Modelo' && valor.name !== 'Año' && valor.name !== 'Marca2' && valor.name !== 'Marca' && valor.name !== 'Código de Aplicación') && (
-                        <div className={handles['FilterDetails--detailsItem-2']} key={`${valor.name}-${valor.value}-${i}`}>
-                            <p className={handles['FilterDetails--text-1']}>{valor.name}</p>
-                            <p className={handles['FilterDetails--text-2']}>{valor.values}</p>
-                        </div>  
-                    )
-                ))
-            } 
-
-        }
-
-        return null
-    }
-
-
+    const {product} = useContext(ProductContext);
+    
+   
+    let grupoName = product.properties;
+    console.log(grupoName);
+    return (
+    <div>
+        {grupoName.map((el)=>(
+            <div className={styles.FilterDetailsContainer}>
+                <span>{el.name}</span>
+                <span>{el.values[0]}</span>
+           </div>
+           
+          
+               
+            
+            
+        ))}
+    </div>);
 }
 
 FilterDetails.getSchema = props => {
